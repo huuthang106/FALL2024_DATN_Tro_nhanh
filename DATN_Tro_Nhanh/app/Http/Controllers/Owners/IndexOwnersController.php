a91c91a1597026e6f7b1b967f5a6131714375ad0
Bun194 <0109duynguyen@gmail.com>
2024-08-12 19:07:38 +0700

    [Font-end_N12] - CatGiaoDienINVOICEV-VietRoute
    [Font-end_N12] - Cắt giao diện profile,  tách đổi mật khẩu ra
    thêm 1 trang viết route

--- DATN_Tro_Nhanh/app/Http/Controllers/Owners/IndexOwnersController.php
+++ DATN_Tro_Nhanh/app/Http/Controllers/Owners/IndexOwnersController.php
@@ -8,4 +8,20 @@ use Illuminate\Http\Request;
 class IndexOwnersController extends Controller
 {
     //
+    public function indexInvoice()
+    {
+        return view('owners.show.dashboard-invoice-listing');
+    }
+    public function editInvoice()
+    {
+        return view('owners.edit.dashboard-edit-invoice');
+    }
+    public function createInvoice()
+    {
+        return view('owners.create.dashboard-add-new-invoice');
+    }
+    public function previewInvoice()
+    {
+        return view('owners.show.dashboard-preview-invoice');
+    }
 }
---
+++ DATN_Tro_Nhanh/routes/owners/room-owner.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Owners\IndexOwnersController;
+
+// Route::get('danh-sach-hoa-don', [InvoiceAdminController::class, 'index'])->name('invoice-listing');
+Route::group(['prefix' => 'danh-sach-hoa-don', 'as' => 'invoice.'], function () {
+    Route::get('/', [IndexOwnersController::class, 'indexInvoice'])->name('invoice-listing');
+    Route::get('chinh-sua-hoa-don', [IndexOwnersController::class, 'editInvoice'])->name('invoice-edit');
+    Route::get('them-moi-hoa-don', [IndexOwnersController::class, 'createInvoice'])->name('invoice-create');
+    Route::get('xem-truoc-hoa-don', [IndexOwnersController::class, 'previewInvoice'])->name('invoice-preview');
+});
