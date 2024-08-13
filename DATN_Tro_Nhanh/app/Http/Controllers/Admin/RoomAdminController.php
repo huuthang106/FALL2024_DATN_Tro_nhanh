91d0fe576b0bcebc4424776a29114565afbc9f71
huuthang106 <thangcachep106@gmail.com>
2024-08-13 17:06:08 +0700

    [update]-giao-dien-admin

--- DATN_Tro_Nhanh/app/Http/Controllers/Admin/RoomAdminController.php
+++ DATN_Tro_Nhanh/app/Http/Controllers/Admin/RoomAdminController.php
@@ -8,4 +8,7 @@ use Illuminate\Http\Request;
 class RoomAdminController extends Controller
 {
     //
+    public function index(){
+        return view('admincp.demo8.dist.index');
+    }
 }
---
+++ DATN_Tro_Nhanh/resources/views/layouts/admin.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    @stack('styleAdmin')
+</head>
+
+<body class="font-sans antialiased">
+    <!--begin::Main-->
+    <!--begin::Root-->
+    <div class="d-flex flex-column flex-root">
+        <!--begin::Page-->
+        <div class="page d-flex flex-row flex-column-fluid">
+            <!--begin::Aside-->
+            <x-navbar-admin />
+            <!--end::Header-->
+            @yield('contentAdmin')
+</body>
+@stack('scriptsAdmin')
+
+</html>
