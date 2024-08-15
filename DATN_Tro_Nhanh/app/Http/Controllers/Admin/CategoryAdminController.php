27f9087e097d707c1c4e4f1c80fcd4efe123893f
MHuy12 <145202827+MHuy12@users.noreply.github.com>
2024-08-15 12:17:15 +0700

    [front-end]-ADMIN_add_edit_category

--- DATN_Tro_Nhanh/app/Http/Controllers/Admin/CategoryAdminController.php
+++ DATN_Tro_Nhanh/app/Http/Controllers/Admin/CategoryAdminController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class CategoryAdminController extends Controller
 {
-    //
+    public function index(){}
+    
+    public function create(){
+        return view('admincp.create.addCategory');
+    }
+    
+    public function update(){
+        return view('admincp.edit.updateCategory');
+}
 }
