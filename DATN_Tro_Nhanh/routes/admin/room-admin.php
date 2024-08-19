bca222c76ae9e5b13b72406abc25b012cd4d9853
huuthang106 <thangcachep106@gmail.com>
2024-08-19 23:55:58 +0700

    [update]-update-room-admin

--- DATN_Tro_Nhanh/routes/admin/room-admin.php
+++ DATN_Tro_Nhanh/routes/admin/room-admin.php
@@ -9,12 +9,10 @@ Route::prefix('')->group(function () {
     Route::get('/them-phong', [RoomAdminController::class, 'add_room'])->name('add-room');
     Route::get('/chinh-sua-phong', [RoomAdminController::class, 'update_room'])->name('update-room');
 });
-Route::prefix('room')->group(function () {
-    Route::get('/xem-tro', [RoomAdminController::class, 'show_room'])->name('show-room');
+Route::prefix('')->group(function () {
+    Route::get('/danh-sach', [RoomAdminController::class, 'show_room'])->name('show-room');
     Route::get('/them-tro', [RoomAdminController::class, 'add_room_show'])->name('add-room-show');
     Route::post('/them-du-lieu-tro', [RoomAdminController::class, 'add_room'])->name('add-room');
-    Route::get('/update-room', [RoomAdminController::class, 'update_room'])->name('update-room');
-
     Route::get('/chinh-sua-tro/{slug}', [RoomAdminController::class, 'update_room_show'])->name('update-room-show');
     Route::put('/chinh-du-lieu-tro/{slug}', [RoomAdminController::class, 'update_room'])->name('update-room');
 });
