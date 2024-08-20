dce9a18c1ce58bbcca889cd37ada69b51fd1027f
huuthang106 <thangcachep106@gmail.com>
2024-08-20 23:45:33 +0700

    [fix]commit-20-82024

--- DATN_Tro_Nhanh/resources/views/components/navbar-admin.blade.php
+++ DATN_Tro_Nhanh/resources/views/components/navbar-admin.blade.php
@@ -1213,13 +1213,75 @@
                             <!--end::Svg Icon-->
                         </span>
                         <span class="menu-title">Zone</span>
-                        <span class="menu-title">Loại</span>
+                      
                         <span class="menu-arrow"></span>
                     </span>
                     <div class="menu-sub menu-sub-accordion menu-active-bg">
                         <div class="menu-item">
                           
                             <a class="menu-link" href="{{ route('admin.danh-sach-khutro') }}">
+              
+                                <span class="menu-bullet">
+                                    <span class="bullet bullet-dot"></span>
+                                </span>
+                                <span class="menu-title">Danh sách </span>
+                            </a>
+                        </div>
+                        {{-- <div class="menu-item">
+                            <a class="menu-link" href="{{ route('admin.add-category') }}">
+                                <span class="menu-bullet">
+                                    <span class="bullet bullet-dot"></span>
+                                </span>
+                                <span class="menu-title">Thêm </span>
+                            </a>
+                        </div>
+                        <div class="menu-item">
+                            <a class="menu-link"
+                                href="{{ route('admin.update-category', ['id' => $category->id]) }}">
+                                <span class="menu-bullet">
+                                    <span class="bullet bullet-dot"></span>
+                                </span>
+                                <span class="menu-title">Chỉnh sửa</span>
+                            </a>
+                        </div> --}}
+                        <div class="menu-item">
+                            <a class="menu-link" href="../../demo8/dist/widgets/mixed.html">
+                                <span class="menu-bullet">
+                                    <span class="bullet bullet-dot"></span>
+                                </span>
+                                <span class="menu-title">
+                                    Thùng rác</span>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+                <div data-kt-menu-trigger="click" class="menu-item menu-accordion">
+                    <span class="menu-link">
+                        <span class="menu-icon">
+                            <!--begin::Svg Icon | path: icons/duotune/general/gen025.svg-->
+                            <span class="svg-icon svg-icon-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    viewBox="0 0 24 24" fill="none">
+                                    <rect x="2" y="2" width="9" height="9" rx="2" fill="black" />
+                                    <rect opacity="0.3" x="13" y="2" width="9" height="9" rx="2"
+                                        fill="black" />
+                                    <rect opacity="0.3" x="13" y="13" width="9" height="9" rx="2"
+                                        fill="black" />
+                                    <rect opacity="0.3" x="2" y="13" width="9" height="9" rx="2"
+                                        fill="black" />
+                                </svg>
+                            </span>
+                            <!--end::Svg Icon-->
+                        </span>
+                   
+                        <span class="menu-title">Loại</span>
+                        <span class="menu-arrow"></span>
+                    </span>
+                    <div class="menu-sub menu-sub-accordion menu-active-bg">
+                        <div class="menu-item">
+                          
+                           
                             <a class="menu-link" href="{{ route('admin.list-category') }}">
                                 <span class="menu-bullet">
                                     <span class="bullet bullet-dot"></span>
