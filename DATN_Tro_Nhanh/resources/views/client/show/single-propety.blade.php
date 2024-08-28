61066774fad7c44e4b60148436e5d22662f0a348
Bun194 <0109duynguyen@gmail.com>
2024-08-28 13:07:57 +0700

    [back-end-N148] ChiTietNguoiDangTinTrongChiTietTro
    [back-end-N148] chi biết room-> xem chi tiết người đăng -> chi tiết người đăng đó

--- DATN_Tro_Nhanh/resources/views/client/show/single-propety.blade.php
+++ DATN_Tro_Nhanh/resources/views/client/show/single-propety.blade.php
@@ -20,78 +20,79 @@
                                         <option>Nhà Phố</option>
                                         <option>Nhà Dành Cho Nhiều Gia Đình</option>
                                     </select>
-                                </div> 
+                                </div>
                                 <div class="col-md-6 pl-md-3 pt-md-0 pt-6">
                                     <label
                                         class="text-uppercase font-weight-500 opacity-7 text-white letter-spacing-093 mb-1">Vị
                                         Trí</label>
-                                    <select 
+                                    <select
                                         class="form-control selectpicker bg-transparent border-bottom rounded-0 border-input-opacity-02"
-                                        name="province" id="city-province" title="Lựa chọn" data-style="p-0 h-24 lh-17 text-white">
+                                        name="province" id="city-province" title="Lựa chọn"
+                                        data-style="p-0 h-24 lh-17 text-white">
                                         <option value='0'>Chọn Tỉnh/Thành Phố...</option>
-                                                <option value='01'>Thành phố Hà Nội</option>
-                                                <option value='79'>Thành phố Hồ Chí Minh</option>
-                                                <option value='31'>Thành phố Hải Phòng</option>
-                                                <option value='48'>Thành phố Đà Nẵng</option>
-                                                <option value='92'>Thành phố Cần Thơ</option>
-                                                <option value='02'>Tỉnh Hà Giang</option>
-                                                <option value='04'>Tỉnh Cao Bằng</option>
-                                                <option value='06'>Tỉnh Bắc Kạn</option>
-                                                <option value='08'>Tỉnh Tuyên Quang</option>
-                                                <option value='10'>Tỉnh Lào Cai</option>
-                                                <option value='11'>Tỉnh Điện Biên</option>
-                                                <option value='12'>Tỉnh Lai Châu</option>
-                                                <option value='14'>Tỉnh Sơn La</option>
-                                                <option value='15'>Tỉnh Yên Bái</option>
-                                                <option value='17'>Tỉnh Hoà Bình</option>
-                                                <option value='19'>Tỉnh Thái Nguyên</option>
-                                                <option value='20'>Tỉnh Lạng Sơn</option>
-                                                <option value='22'>Tỉnh Quảng Ninh</option>
-                                                <option value='24'>Tỉnh Bắc Giang</option>
-                                                <option value='25'>Tỉnh Phú Thọ</option>
-                                                <option value='26'>Tỉnh Vĩnh Phúc</option>
-                                                <option value='27'>Tỉnh Bắc Ninh</option>
-                                                <option value='30'>Tỉnh Hải Dương</option>
-                                                <option value='33'>Tỉnh Hưng Yên</option>
-                                                <option value='34'>Tỉnh Thái Bình</option>
-                                                <option value='35'>Tỉnh Hà Nam</option>
-                                                <option value='36'>Tỉnh Nam Định</option>
-                                                <option value='37'>Tỉnh Ninh Bình</option>
-                                                <option value='38'>Tỉnh Thanh Hóa</option>
-                                                <option value='40'>Tỉnh Nghệ An</option>
-                                                <option value='42'>Tỉnh Hà Tĩnh</option>
-                                                <option value='44'>Tỉnh Quảng Bình</option>
-                                                <option value='45'>Tỉnh Quảng Trị</option>
-                                                <option value='46'>Tỉnh Thừa Thiên Huế</option>
-                                                <option value='49'>Tỉnh Quảng Nam</option>
-                                                <option value='51'>Tỉnh Quảng Ngãi</option>
-                                                <option value='52'>Tỉnh Bình Định</option>
-                                                <option value='54'>Tỉnh Phú Yên</option>
-                                                <option value='56'>Tỉnh Khánh Hòa</option>
-                                                <option value='58'>Tỉnh Ninh Thuận</option>
-                                                <option value='60'>Tỉnh Bình Thuận</option>
-                                                <option value='62'>Tỉnh Kon Tum</option>
-                                                <option value='64'>Tỉnh Gia Lai</option>
-                                                <option value='66'>Tỉnh Đắk Lắk</option>
-                                                <option value='67'>Tỉnh Đắk Nông</option>
-                                                <option value='68'>Tỉnh Lâm Đồng</option>
-                                                <option value='70'>Tỉnh Bình Phước</option>
-                                                <option value='72'>Tỉnh Tây Ninh</option>
-                                                <option value='74'>Tỉnh Bình Dương</option>
-                                                <option value='75'>Tỉnh Đồng Nai</option>
-                                                <option value='77'>Tỉnh Bà Rịa - Vũng Tàu</option>
-                                                <option value='80'>Tỉnh Long An</option>
-                                                <option value='82'>Tỉnh Tiền Giang</option>
-                                                <option value='83'>Tỉnh Bến Tre</option>
-                                                <option value='84'>Tỉnh Trà Vinh</option>
-                                                <option value='86'>Tỉnh Vĩnh Long</option>
-                                                <option value='87'>Tỉnh Đồng Tháp</option>
-                                                <option value='89'>Tỉnh An Giang</option>
-                                                <option value='91'>Tỉnh Kiên Giang</option>
-                                                <option value='93'>Tỉnh Hậu Giang</option>
-                                                <option value='94'>Tỉnh Sóc Trăng</option>
-                                                <option value='95'>Tỉnh Bạc Liêu</option>
-                                                <option value='96'>Tỉnh Cà Mau</option>
+                                        <option value='01'>Thành phố Hà Nội</option>
+                                        <option value='79'>Thành phố Hồ Chí Minh</option>
+                                        <option value='31'>Thành phố Hải Phòng</option>
+                                        <option value='48'>Thành phố Đà Nẵng</option>
+                                        <option value='92'>Thành phố Cần Thơ</option>
+                                        <option value='02'>Tỉnh Hà Giang</option>
+                                        <option value='04'>Tỉnh Cao Bằng</option>
+                                        <option value='06'>Tỉnh Bắc Kạn</option>
+                                        <option value='08'>Tỉnh Tuyên Quang</option>
+                                        <option value='10'>Tỉnh Lào Cai</option>
+                                        <option value='11'>Tỉnh Điện Biên</option>
+                                        <option value='12'>Tỉnh Lai Châu</option>
+                                        <option value='14'>Tỉnh Sơn La</option>
+                                        <option value='15'>Tỉnh Yên Bái</option>
+                                        <option value='17'>Tỉnh Hoà Bình</option>
+                                        <option value='19'>Tỉnh Thái Nguyên</option>
+                                        <option value='20'>Tỉnh Lạng Sơn</option>
+                                        <option value='22'>Tỉnh Quảng Ninh</option>
+                                        <option value='24'>Tỉnh Bắc Giang</option>
+                                        <option value='25'>Tỉnh Phú Thọ</option>
+                                        <option value='26'>Tỉnh Vĩnh Phúc</option>
+                                        <option value='27'>Tỉnh Bắc Ninh</option>
+                                        <option value='30'>Tỉnh Hải Dương</option>
+                                        <option value='33'>Tỉnh Hưng Yên</option>
+                                        <option value='34'>Tỉnh Thái Bình</option>
+                                        <option value='35'>Tỉnh Hà Nam</option>
+                                        <option value='36'>Tỉnh Nam Định</option>
+                                        <option value='37'>Tỉnh Ninh Bình</option>
+                                        <option value='38'>Tỉnh Thanh Hóa</option>
+                                        <option value='40'>Tỉnh Nghệ An</option>
+                                        <option value='42'>Tỉnh Hà Tĩnh</option>
+                                        <option value='44'>Tỉnh Quảng Bình</option>
+                                        <option value='45'>Tỉnh Quảng Trị</option>
+                                        <option value='46'>Tỉnh Thừa Thiên Huế</option>
+                                        <option value='49'>Tỉnh Quảng Nam</option>
+                                        <option value='51'>Tỉnh Quảng Ngãi</option>
+                                        <option value='52'>Tỉnh Bình Định</option>
+                                        <option value='54'>Tỉnh Phú Yên</option>
+                                        <option value='56'>Tỉnh Khánh Hòa</option>
+                                        <option value='58'>Tỉnh Ninh Thuận</option>
+                                        <option value='60'>Tỉnh Bình Thuận</option>
+                                        <option value='62'>Tỉnh Kon Tum</option>
+                                        <option value='64'>Tỉnh Gia Lai</option>
+                                        <option value='66'>Tỉnh Đắk Lắk</option>
+                                        <option value='67'>Tỉnh Đắk Nông</option>
+                                        <option value='68'>Tỉnh Lâm Đồng</option>
+                                        <option value='70'>Tỉnh Bình Phước</option>
+                                        <option value='72'>Tỉnh Tây Ninh</option>
+                                        <option value='74'>Tỉnh Bình Dương</option>
+                                        <option value='75'>Tỉnh Đồng Nai</option>
+                                        <option value='77'>Tỉnh Bà Rịa - Vũng Tàu</option>
+                                        <option value='80'>Tỉnh Long An</option>
+                                        <option value='82'>Tỉnh Tiền Giang</option>
+                                        <option value='83'>Tỉnh Bến Tre</option>
+                                        <option value='84'>Tỉnh Trà Vinh</option>
+                                        <option value='86'>Tỉnh Vĩnh Long</option>
+                                        <option value='87'>Tỉnh Đồng Tháp</option>
+                                        <option value='89'>Tỉnh An Giang</option>
+                                        <option value='91'>Tỉnh Kiên Giang</option>
+                                        <option value='93'>Tỉnh Hậu Giang</option>
+                                        <option value='94'>Tỉnh Sóc Trăng</option>
+                                        <option value='95'>Tỉnh Bạc Liêu</option>
+                                        <option value='96'>Tỉnh Cà Mau</option>
                                     </select>
                                 </div>
                             </div>
@@ -184,10 +185,10 @@
                                 <div class="position-absolute pos-fixed-top-right z-index-3">
                                     <ul class="list-inline pt-4 pr-5">
                                         <li class="list-inline-item mr-2">
-                                            <a href="{{ route('client.add.favourite', ['slug' => $rooms->slug]) }}" 
+                                            <a href="{{ route('client.add.favourite', ['slug' => $rooms->slug]) }}"
                                                 class="w-40px h-40 border rounded-circle d-inline-flex align-items-center justify-content-center">
-                                                 <i class="fas fa-heart"></i>
-                                             </a>
+                                                <i class="fas fa-heart"></i>
+                                            </a>
                                         </li>
                                         <li class="list-inline-item mr-2">
                                             <button type="button"
@@ -1258,10 +1259,10 @@
                                             <p class="fs-17 font-weight-bold text-heading mb-0">$1.250.000</p>
                                             <ul class="list-inline mb-0">
                                                 <li class="list-inline-item mr-2">
-                                                    <a href="{{ route('client.add.favourite', ['slug' => $rooms->slug]) }}" 
+                                                    <a href="{{ route('client.add.favourite', ['slug' => $rooms->slug]) }}"
                                                         class="w-40px h-40 border rounded-circle d-inline-flex align-items-center justify-content-center">
-                                                         <i class="fas fa-heart"></i>
-                                                     </a>
+                                                        <i class="fas fa-heart"></i>
+                                                    </a>
                                                 </li>
                                                 <li class="list-inline-item">
                                                     <a href="#"
@@ -1368,7 +1369,8 @@
                                                             <i class="far fa-images"></i><span class="pl-1">9</span>
                                                         </a>
                                                     </li>
-                                                    <li class="list-inline-item" data-toggle="tooltip" title="2 Video">
+                                                    <li class="list-inline-item" data-toggle="tooltip"
+                                                        title="2 Video">
                                                         <a href="#" class="text-white hover-primary">
                                                             <i class="far fa-play-circle"></i><span
                                                                 class="pl-1">2</span>
@@ -1499,11 +1501,11 @@
                                         </div>
                                     </div>
                                 </div>
-                                <p class="mb-6 mt-1">{{ $rooms->description }}</p>
+                                {{-- <p class="mb-6 mt-1">{{ $rooms->description }}</p> --}}
                                 <div class="mr-xl-2">
-                                    <a href="#"
-                                        class="btn btn-outline-primary btn-lg btn-block rounded border text-body border-hover-primary hover-white">Lên
-                                        lịch một chuyến tham quan</a>
+                                    <a href="{{ route('client.client-agent-detail', ['slug' => $user->slug]) }}"
+                                        class="btn btn-outline-primary btn-lg btn-block rounded border text-body border-hover-primary hover-white">Người
+                                        đăng: {{ $user->name }}</a>
                                     <a href="#"
                                         class="btn btn-outline-primary btn-lg btn-block rounded border text-body border-hover-primary hover-white mt-4">Yêu
                                         cầu thông tin</a>
