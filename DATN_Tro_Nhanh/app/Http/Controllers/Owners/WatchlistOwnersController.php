3cdc60aca7be1323af606f559cd5c8582ef7fc35
huuthang106 <thangcachep106@gmail.com>
2024-09-24 16:50:41 +0700

    bo-sung-live-wire-trang-followings

--- DATN_Tro_Nhanh/app/Http/Controllers/Owners/WatchlistOwnersController.php
+++ DATN_Tro_Nhanh/app/Http/Controllers/Owners/WatchlistOwnersController.php
@@ -20,7 +20,7 @@ class WatchlistOwnersController extends Controller
         if(Auth::check()){
             $user_id= Auth::id();
         $myFollowings = $this->watchListOwner->myFollowings($user_id, self::limit);
-        return view('owners.show.my-watch-list',compact('myFollowings'));
+        return view('owners.show.my-watch-list',compact('myFollowings',));
 
         }
     }
@@ -28,9 +28,9 @@ class WatchlistOwnersController extends Controller
     public function is_following(){
         if(Auth::check()){
             $user_id= Auth::id();
-        $myFollowings = $this->watchListOwner->myFollowers($user_id, self::limit);
+        // $myFollowings = $this->watchListOwner->myFollowers($user_id, self::limit);
         // dd($myFollowings);
-        return view('owners.show.my_followers',compact('myFollowings'));
+        return view('owners.show.my_followers',compact('user_id'));
 
         }
     }
--- DATN_Tro_Nhanh/resources/views/owners/show/my_followers.blade.php
+++ DATN_Tro_Nhanh/resources/views/owners/show/my_followers.blade.php
@@ -1,165 +1,7 @@
 @extends('layouts.owner')
 @section('titleOwner', 'Lượt Theo Dõi Của Tôi | TRỌ NHANH')
 @section('contentOwners')
-    <main id="content" class="bg-gray-01">
-        <div class="px-3 px-lg-6 px-xxl-13 py-5 py-lg-10 invoice-listing">
-            <div class="mb-6">
-                <div class="row">
-                    <div class="col-sm-12 col-md-6 d-flex justify-content-md-start justify-content-center">
-                        <div class="d-flex form-group mb-0 align-items-center">
-                            <label for="invoice-list_length" class="d-block mr-2 mb-0">Kết quả:</label>
-                            <select name="invoice-list_length" id="invoice-list_length" aria-controls="invoice-list"
-                                class="form-control form-control-lg mr-2 selectpicker"
-                                data-style="bg-white btn-lg h-52 py-2 border">
-                                <option value="7">7</option>
-                                <option value="10">10</option>
-                                <option value="20">20</option>
-                                <option value="50">50</option>
-                            </select>
-                        </div>
-
-                    </div>
-                    <div class="col-sm-12 col-md-6 d-flex justify-content-md-end justify-content-center mt-md-0 mt-3">
-                        <div class="input-group input-group-lg bg-white mb-0 position-relative mr-2">
-                            <input type="text" class="form-control bg-transparent border-1x" placeholder="Tìm kiếm..."
-                                aria-label="" aria-describedby="basic-addon1">
-                            <div class="input-group-append position-absolute pos-fixed-right-center">
-                                <button class="btn bg-transparent border-0 text-gray lh-1" type="button"><i
-                                        class="fal fa-search"></i></button>
-                            </div>
-                        </div>
-                        <div class="align-self-center">
-                            <button class="btn btn-danger btn-lg" tabindex="0"
-                                aria-controls="invoice-list"><span>Xóa</span></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="table-responsive">
-            <table id="notification-list" class="table table-hover bg-white border rounded-lg">
-                <thead>
-                    <tr role="row">
-                        <th class="no-sort py-6 pl-6">
-                            <label class="new-control new-checkbox checkbox-primary m-auto">
-                                <input type="checkbox" class="new-control-input chk-parent select-customers-info">
-                            </label>
-                        </th>
-                        <th class="py-6">Hình ảnh</th>
-
-                        <th class="py-6">Tên</th>
-                        <th class="py-6">Chức năng</th>
-                        <th class="py-6">Thao tác</th>
-
-
-                    </tr>
-                </thead>
-                <tbody>
-                    @if ($myFollowings->isEmpty())
-                        <tr>
-                            <td colspan="8" class="text-center">Danh sách trống</td>
-                        </tr>
-                    @else
-                        @foreach ($myFollowings as $item)
-                            <tr role="row">
-                                <td class="checkbox-column py-6 pl-6">
-                                    <label class="new-control new-checkbox checkbox-primary m-auto">
-                                        <input type="checkbox" class="new-control-input child-chk select-customers-info">
-                                    </label>
-                                </td>
-                                <td class="align-middle pt-6 pb-4 px-6">
-                                    <div class="media d-flex align-items-center">
-                                        <div class="w-120px mr-4 position-relative">
-                                            <a href="{{ route('owners.show-blog', $item->followers->slug) }}">
-                                                @if ($item->followers->image)
-                                                    <img src="{{ asset('assets/images/' . $item->followers->image) }}"
-                                                        alt="{{ $item->followers->image }}" class="img-fluid rounded-image">
-                                                @else
-                                                    <p>No images available</p>
-                                                @endif
-                                            </a>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="align-middle">
-                                    {{ $item->followers->name }}
-                                </td>
-                                <td class="align-middle">
-                                    @if ($item->followers->role == '2')
-                                        <span>Người đưa tin</span>
-                                    @elseif ($item->followers->role == '0')
-                                        <span>Người quản trị</span>
-                                    @else
-                                        <span>Người dùng</span>
-                                    @endif
-
-                                </td>
-
-                                <td class="align-middle">
-                                    <a href="#" data-toggle="tooltip" title="Xóa"
-                                        class="d-inline-block fs-18 text-muted hover-primary">
-                                        <i class="fal fa-trash-alt"></i>
-                                    </a>
-                                </td>
-                            </tr>
-                            {{-- 1 --}}
-                        @endforeach
-                    @endif
-                </tbody>
-            </table>
-            <!-- Phân trang 1-->
-            @if ($myFollowings->hasPages())
-                <div class="d-flex justify-content-center mt-4">
-                    @if ($myFollowings->hasPages())
-                        @php
-                            $queryParams = [
-                                'query' => request()->query('query', ''),
-                                'notification-list_length' => request()->query('notification-list_length', 10),
-                            ];
-                        @endphp
-                        <nav aria-label="Page navigation">
-                            <ul class="pagination">
-                                @if ($myFollowings->onFirstPage())
-                                    <li class="page-item disabled"><span class="page-link">&laquo;</span></li>
-                                @else
-                                    <li class="page-item">
-                                        <a class="page-link"
-                                            href="{{ $myFollowings->appends($queryParams)->previousPageUrl() }}"
-                                            rel="prev">&laquo;</a>
-                                    </li>
-                                @endif
-                                @for ($i = max(1, $myFollowings->currentPage() - 2); $i <= min($myFollowings->lastPage(), $myFollowings->currentPage() + 2); $i++)
-                                    <li class="page-item {{ $myFollowings->currentPage() == $i ? 'active' : '' }}">
-                                        <a class="page-link"
-                                            href="{{ $myFollowings->appends($queryParams)->url($i) }}">{{ $i }}</a>
-                                    </li>
-                                @endfor
-                                @if ($myFollowings->hasMorePages())
-                                    <li class="page-item">
-                                        <a class="page-link"
-                                            href="{{ $myFollowings->appends($queryParams)->nextPageUrl() }}"
-                                            rel="next">&raquo;</a>
-                                    </li>
-                                @else
-                                    <li class="page-item disabled"><span class="page-link">&raquo;</span></li>
-                                @endif
-                            </ul>
-                        </nav>
-                    @endif
-                </div>
-                {{-- Kết quả --}}
-                <div class="text-center mt-2">
-                    @if ($myFollowings->total() > 0)
-                        {{ $myFollowings->firstItem() }}-{{ $myFollowings->lastItem() }} trong
-                        {{ $myFollowings->total() }} Kết quả
-                    @else
-                        Không có kết quả nào
-                    @endif
-                </div>
-            @endif
-        </div>
-        </div>
-    </main>
+@livewire('followings', ['id' => $user_id]) <!-- Thay đổi $userId thành ID người dùng thực tế -->
 @endsection
 @push('styleOwners')
     {{-- <meta charset="utf-8">
@@ -272,4 +114,13 @@
     <script src="{{ asset('assets/js/theme.js') }}"></script>
     {{-- Notification - Pagination --}}
     <script src="{{ asset('assets/js/notification-list/notification-pagination.js') }}"></script>
+    <script>document.addEventListener('livewire:load', function () {
+        // Khởi tạo selectpicker
+        $('.selectpicker').selectpicker();
+    
+        // Cập nhật selectpicker khi Livewire cập nhật
+        Livewire.hook('message.processed', (message, component) => {
+            $('.selectpicker').selectpicker('refresh');
+        });
+    });</script>
 @endpush
