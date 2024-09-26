ca116645327c620269f6fa1c5a5bdfe970e27050
huuthang106 <thangcachep106@gmail.com>
2024-09-26 15:35:56 +0700

    update-zalo-app

--- DATN_Tro_Nhanh/resources/views/my-react-app/tro-nhanh/public/assets/js/ntt-script.js
+++
@@ -1,8 +0,0 @@
-function toggleDropdown(id) {
-    const element = document.getElementById(id);
-    if (element.classList.contains('show')) {
-        element.classList.remove('show');
-    } else {
-        element.classList.add('show');
-    }
-}
