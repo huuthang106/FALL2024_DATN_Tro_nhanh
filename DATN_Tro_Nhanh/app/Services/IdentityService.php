1171ac33e1c4924e7026f3025d2b30437026f95f
huuthang106 <thangcachep106@gmail.com>
2024-10-12 16:01:42 +0700

    fix-commit

--- DATN_Tro_Nhanh/app/Services/IdentityService.php
+++ DATN_Tro_Nhanh/app/Services/IdentityService.php
@@ -391,9 +391,9 @@ class IdentityService
 
         return redirect()->back()->with(['error' => $errorMessages['default'], 'showAlert' => true]);
     }
-    public function getIdIdentity($user_id)
-    {
-        $identity = Identity::where('user_id', $user_id)->first();
-        return $identity ? $identity->id : null;
-    }
+    // public function getIdIdentity($user_id)
+    // {
+    //     $identity = Identity::where('user_id', $user_id)->first();
+    //     return $identity ? $identity->id : null;
+    // }
 }
